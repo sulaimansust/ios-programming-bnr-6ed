38389849a1a36237b15f5632594c02114f67b154
Laurent Boileau <developer@laurentboileau.ca>
2017-05-12 23:52:27 -0400

    2. The Swift Language

---
+++ The Swift Language.playground/Contents.swift
@@ -0,0 +1,62 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+str = "Hello, Swift"
+let constStr = str
+
+var nextYear: Int
+var bodyTemp: Float
+var hasPet: Bool
+var arrayOfInts: [Int]
+var dictionaryOfCapitalsByCountry: [String:String]
+var winningLotteryNumbers: Set<Int>
+let number = 42
+let fmStation = 91.1
+var countingUp = ["one", "two"]
+let secondElement = countingUp[1]
+countingUp.count
+countingUp.append("three")
+let nameByParkingSpace = [13: "Alice", 27: "Bob"]
+if let space13Assignee: String? = nameByParkingSpace[13] {
+    print("Key 13 is assigned in the dictionary!")
+}
+let space42Assignee: String? = nameByParkingSpace[42]
+let emptyString = String()
+emptyString.isEmpty
+let emptyArrayOfInts = [Int]()
+let emptySetOfFloats = Set<Float>()
+let defaultNumber = Int()
+let defaultBool = Bool()
+let meaningOfLife = String(number)
+let availableRooms = Set([205, 411, 412])
+let defaultFloat = Float()
+let floatFromLiteral = Float(3.14)
+let easyPi = 3.14
+let floatFromDouble = Float(easyPi)
+let floatingPi: Float = 3.14
+var anOptionalFloat: Float?
+var anOptionalArrayOfStrings: [String]?
+var anOptionalArrayOfOptionalStrings: [String?]?
+
+var reading1: Float?
+var reading2: Float?
+var reading3: Float?
+
+reading1 = 9.8
+reading2 = 9.2
+reading3 = 9.7
+
+if let r1 = reading1,
+    let r2 = reading2,
+    let r3 = reading3 {
+    let avgRating = (r1 + r2 + r3) / 3
+} else {
+    let errorString = "Instrument reported a reading that was nil."
+}
+
+let range = 0..<countingUp.count
+for (space, name) in nameByParkingSpace {
+    let permit = "Space \(space): \(name)"
+}
