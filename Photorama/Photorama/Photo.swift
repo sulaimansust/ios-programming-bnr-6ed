e5418a480869ce221972f878167b248a0bb102bc
Laurent Boileau <developer@laurentboileau.ca>
2017-06-07 17:43:03 -0400

    20. Parse JSON

---
+++ Photorama/Photorama/Photo.swift
@@ -0,0 +1,25 @@
+//
+//  Photo.swift
+//  Photorama
+//
+//  Created by Laurent Boileau on 2017-06-07.
+//  Copyright © 2017 Big Nerd Ranch. All rights reserved.
+//
+
+import Foundation
+
+class Photo {
+
+    let title: String
+    let remoteURL: URL
+    let photoID: String
+    let dateTaken: Date
+
+    init(title: String, photoID: String, remoteURL: URL, dateTaken: Date) {
+        self.title = title
+        self.photoID = photoID
+        self.remoteURL = remoteURL
+        self.dateTaken = dateTaken
+    }
+
+}
--- Photorama/Photorama/PhotosViewController.swift
+++ Photorama/Photorama/PhotosViewController.swift
@@ -17,7 +17,14 @@ class PhotosViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        store.fetchInterestingPhotos()
+        store.fetchInterestingPhotos { (photosResult) -> Void in
+            switch photosResult {
+            case let .success(photos):
+                print("Successfully found \(photos.count) photos.")
+            case let .failure(error):
+                print("Error fetching interesting photos: \(error)")
+            }
+        }
     }
 
 }
