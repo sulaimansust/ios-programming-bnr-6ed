d497084be6bdaf4e9ca8c1d84d5e413455a063ca
Laurent Boileau <developer@laurentboileau.ca>
2017-06-07 18:09:49 -0400

    20. Web Services

--- Photorama/Photorama/PhotoStore.swift
+++ Photorama/Photorama/PhotoStore.swift
@@ -6,7 +6,16 @@
 //  Copyright © 2017 Big Nerd Ranch. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+enum ImageResult {
+    case success(UIImage)
+    case failure(Error)
+}
+
+enum PhotoError: Error {
+    case imageCreationError
+}
 
 enum PhotosResult {
     case success([Photo])
@@ -25,10 +34,43 @@ class PhotoStore {
         let request = URLRequest(url: url)
         let task = session.dataTask(with: request) { (data, response, error) -> Void in
             let result = self.processPhotosRequest(data: data, error: error)
-            completion(result)
+            OperationQueue.main.addOperation {
+                completion(result)
+            }
         }
         task.resume()
     }
+
+    func fetchImage(for photo: Photo, completion: @escaping (ImageResult) -> Void) {
+        let photoURL = photo.remoteURL
+        let request = URLRequest(url: photoURL)
+
+        let task = session.dataTask(with: request) {
+            (data, response, error) -> Void in
+            let result = self.processImageRequest(data: data, error: error)
+            OperationQueue.main.addOperation {
+                completion(result)
+            }
+        }
+
+        task.resume()
+    }
+
+    private func processImageRequest(data: Data?, error: Error?) -> ImageResult {
+        guard
+            let imageData = data,
+            let image = UIImage(data: imageData)
+        else {
+            if data == nil {
+                return .failure(error!)
+            } else {
+                return .failure(PhotoError.imageCreationError)
+            }
+        }
+
+        return .success(image)
+    }
+
     private func processPhotosRequest(data: Data?, error: Error?) -> PhotosResult {
         guard let jsonData = data else {
             return .failure(error!)
